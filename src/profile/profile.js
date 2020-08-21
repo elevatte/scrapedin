3e3a05cfaa75c3bb1e8ce9e21e41d093f4e99af4
pabrrs <paulo.alfredo.barros@gmail.com>
2020-08-21 08:12:22 -0300

    :fire: Remove unused profile infos

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -9,7 +9,7 @@ const cleanProfileData = require('./cleanProfileData')
 
 const logger = require('../logger')(__filename)
 
-module.exports = async (browser, cookies, url, waitTimeToScrapMs = 500, hasToGetContactInfo = false, puppeteerAuthenticate = undefined) => {
+module.exports = async (browser, cookies, url, waitTimeToScrapMs = 500, hasToGetContactInfo = true, puppeteerAuthenticate = undefined) => {
   logger.info(`starting scraping url: ${url}`)
 
   const page = await openPage({ browser, cookies, url, puppeteerAuthenticate })
@@ -36,21 +36,23 @@ module.exports = async (browser, cookies, url, waitTimeToScrapMs = 500, hasToGet
     await new Promise((resolve) => { setTimeout(() => { resolve() }, waitTimeToScrapMs / 2)})
   }
 
+  // TODO: algumas das sessões abaixo estão comentadas pois não serão utilizadas no mvp
   const [profile] = await scrapSection(page, template.profile)
   const [about] = await scrapSection(page, template.about)
   const positions = await scrapSection(page, template.positions)
   const educations = await scrapSection(page, template.educations)
-  const [recommendationsCount] = await scrapSection(page, template.recommendations_count)
-  const recommendationsReceived = await scrapSection(page, template.recommendations_received)
-  const recommendationsGiven = await scrapSection(page, template.recommendations_given)
+  // const [recommendationsCount] = await scrapSection(page, template.recommendations_count)
+  // const recommendationsReceived = await scrapSection(page, template.recommendations_received)
+  // const recommendationsGiven = await scrapSection(page, template.recommendations_given)
   const skills = await scrapSection(page, template.skills)
-  const accomplishments = await scrapSection(page, template.accomplishments)
+  // const accomplishments = await scrapSection(page, template.accomplishments)
   const courses = await scrapSection(page, template.courses)
   const languages = await scrapAccomplishmentPanel(page, 'languages')
-  const projects = await scrapAccomplishmentPanel(page, 'projects')
+  // const projects = await scrapAccomplishmentPanel(page, 'projects')
   const volunteerExperiences = await scrapSection(page, template.volunteer_experiences)
-  const peopleAlsoViewed = await scrapSection(page, template.people_also_viewed)
-  const contact = hasToGetContactInfo ? await contactInfo(page) : []
+  // const peopleAlsoViewed = await scrapSection(page, template.people_also_viewed)
+
+  const contacts = hasToGetContactInfo ? await contactInfo(page) : []
 
   await page.close()
   logger.info(`finished scraping url: ${url}`)
@@ -61,19 +63,19 @@ module.exports = async (browser, cookies, url, waitTimeToScrapMs = 500, hasToGet
     positions,
     educations,
     skills,
-    recommendations: {
-      givenCount: recommendationsCount ? recommendationsCount.given : 0,
-      receivedCount: recommendationsCount ? recommendationsCount.received : 0,
-      given: recommendationsReceived,
-      received: recommendationsGiven
-    },
-    accomplishments,
     courses,
     languages,
-    projects,
-    peopleAlsoViewed,
     volunteerExperiences,
-    contact
+    contacts
+    // recommendations: {
+    //   givenCount: recommendationsCount ? recommendationsCount.given : 0,
+    //   receivedCount: recommendationsCount ? recommendationsCount.received : 0,
+    //   given: recommendationsReceived,
+    //   received: recommendationsGiven
+    // },
+    // accomplishments,
+    // projects,
+    // peopleAlsoViewed,
   }
 
   const cleanedProfile = cleanProfileData(rawProfile)
