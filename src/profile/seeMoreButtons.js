37307fb42fcb9e991b0535f566ca515131e698a2
pabrrs <paulo.alfredo.barros@gmail.com>
2020-08-21 11:37:50 -0300

    :alembic: Add draft to click in 'see more' buttons

--- src/profile/seeMoreButtons.js
+++ src/profile/seeMoreButtons.js
@@ -2,41 +2,45 @@ const logger = require('../logger')(__filename)
 const seeMoreButtons = [
   {
     id: 'SHOW_MORE_ABOUT',
-    selector: '#line-clamp-show-more-button'
-  },{
+    selector: '#line-clamp-show-more-button',
+  },
+  {
     id: 'SHOW_MORE_EXPERIENCES',
-    selector: '#experience-section .pv-profile-section__see-more-inline'
-  },{
+    selector: '#experience-section .pv-profile-section__see-more-inline',
+  },
+  {
     id: 'SEE_MORE_EXPERIENCES',
-    selector: '#experience-section .inline-show-more-text__button'
-  },{
+    selector: '#experience-section .inline-show-more-text__button',
+  },
+  {
     id: 'SHOW_MORE_CERTIFICATIONS',
-    selector: '#certifications-section .pv-profile-section__see-more-inline'
-  },{
+    selector: '#certifications-section .pv-profile-section__see-more-inline',
+  },
+  {
     id: 'SHOW_MORE_SKILLS',
-    selector: '.pv-skills-section__additional-skills'
-  },{
+    selector: '.pv-skills-section__additional-skills',
+  },
+  {
     id: 'SEE_MORE_RECOMMENDATIONS',
-    selector: '.recommendations-inlining #line-clamp-show-more-button'
-  }
+    selector: '.recommendations-inlining #line-clamp-show-more-button',
+  },
 ]
 
+const clickAll = async (page, buttonSelectors = seeMoreButtons) => {
+  for (const buttonSelector of buttonSelectors) {
+    const buttons = await page.$$(buttonSelector.selector)
+    for (const button of buttons) {
+      if (button) {
+        await button
+          .click()
+          .catch((e) =>
+            logger.warn(`couldn't click on ${button.selector}, it's probably invisible`)
+          )
 
-const clickAll = async(page) => {
-  for(let i = 0; i < seeMoreButtons.length; i++){
-    const button = seeMoreButtons[i]
-    const elems = await page.$$(button.selector)
-
-    for(let j = 0; j < elems.length; j++){
-      const elem = elems[j]
-      if (elem) {
-        await elem.click()
-          .catch((e) => logger.warn(`couldn't click on ${button.selector}, it's probably invisible`))
+        await clickAll(page, [ buttonSelector ])
       }
     }
   }
-
-  return
 }
 
 module.exports = { clickAll }
