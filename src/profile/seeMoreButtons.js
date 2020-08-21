4264915622c9a7deca4c48710cfad94bb9cacc88
pabrrs <paulo.alfredo.barros@gmail.com>
2020-08-21 14:05:47 -0300

    :bug: Fix see-more buttons clicks

--- src/profile/seeMoreButtons.js
+++ src/profile/seeMoreButtons.js
@@ -1,46 +1,61 @@
 const logger = require('../logger')(__filename)
+const wait = require('../wait')
 const seeMoreButtons = [
   {
     id: 'SHOW_MORE_ABOUT',
     selector: '#line-clamp-show-more-button',
+    textMatch: (text) => RegExp(/visualizar mais/i).test(text)
   },
   {
     id: 'SHOW_MORE_EXPERIENCES',
     selector: '#experience-section .pv-profile-section__see-more-inline',
+    textMatch: (text) => RegExp(/exibir mais.*experiências/i).test(text)
+
   },
   {
     id: 'SEE_MORE_EXPERIENCES',
     selector: '#experience-section .inline-show-more-text__button',
+    textMatch: (text) => RegExp(/visualizar mais/i).test(text)
+
   },
   {
     id: 'SHOW_MORE_CERTIFICATIONS',
     selector: '#certifications-section .pv-profile-section__see-more-inline',
+    textMatch: (text) => RegExp(/exibir mais/i).test(text)
   },
   {
     id: 'SHOW_MORE_SKILLS',
     selector: '.pv-skills-section__additional-skills',
-  },
-  {
-    id: 'SEE_MORE_RECOMMENDATIONS',
-    selector: '.recommendations-inlining #line-clamp-show-more-button',
-  },
+    textMatch: (text) => RegExp(/exibir todas as competências de.*/i).test(text)
+  }
+  // {
+  //   id: 'SEE_MORE_RECOMMENDATIONS',
+  //   selector: '.recommendations-inlining #line-clamp-show-more-button',
+  //   textMatch: () => false
+  // },
 ]
 
-const clickAll = async (page, buttonSelectors = seeMoreButtons) => {
+const clickAll = async (page, buttonSelectors = seeMoreButtons, delay = 250) => {
   for (const buttonSelector of buttonSelectors) {
     const buttons = await page.$$(buttonSelector.selector)
     for (const button of buttons) {
       if (button) {
-        await button
+        const innerTextEl = await button.getProperty('innerText')
+        const innerText = innerTextEl._remoteObject.value
+        const shouldClickAgain =  buttonSelector.textMatch(innerText) 
+        console.log({ id: buttonSelector.id, innerText, shouldClickAgain })
+        if (shouldClickAgain) {
+          await button
           .click()
           .catch((e) =>
             logger.warn(`couldn't click on ${button.selector}, it's probably invisible`)
           )
-
-        await clickAll(page, [ buttonSelector ])
+          await clickAll(page, [ buttonSelector ], false)   
+        }
       }
     }
   }
+  return delay ? wait(delay) : true
 }
 
 module.exports = { clickAll }
